813124770490a2775c7810c49c6455d62b150706
郑清华 <450486038@qq.com>
2019-06-21 08:17:06 +0000

    切割头部和尾部视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <a class="navbar-brand" href="/">Weibo App</a>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/help">帮助</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="/about">关于我们</a>
+        </li>
+      </ul>
+      <form class="form-inline mt-2 mt-md-0">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">登陆</button>
+      </form>
+    </div>
+  </nav>
