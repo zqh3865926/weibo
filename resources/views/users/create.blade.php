7ce147978fff1274ca5c96e985c6be149a28b56a
郑清华 <450486038@qq.com>
2019-09-03 09:42:51 +0000

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,20 +1,36 @@
 @extends('layouts.default');
 @section('title','注册');
 @section('content')
-  <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <div class="offset-md-2 col-md-8">
+    <div class="card ">
+      <div class="card-header">
+        <h5>注册</h5>
+      </div>
+      <div class="card-body">
+        <form method="POST" action="{{ route('users.store') }}">
+          <div class="form-group">
+            <label for="name">名称：</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">邮箱：</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">密码：</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">确认密码：</label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">注册</button>
+        </form>
+      </div>
+    </div>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 @stop
