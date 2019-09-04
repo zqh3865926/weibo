e0b6be2d9c6b85998ad4e16e6ec1046da48e2027
郑清华 <450486038@qq.com>
2019-09-04 02:50:30 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts.default');
-@section('title','注册');
+@extends('layouts.default')
+@section('title','注册')
 @section('content')
   <div class="offset-md-2 col-md-8">
     <div class="card ">
       <div class="card-header">
         <h5>注册</h5>
       </div>
+      @include('shared._errors')
       <div class="card-body">
         <form method="POST" action="{{ route('users.store') }}">
           {{ csrf_field() }}
