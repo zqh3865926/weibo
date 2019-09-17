cc4ef29d2412adebd9732c1c76ef370671e2591f
郑清华 <450486038@qq.com>
2019-09-17 07:59:28 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -16,7 +16,7 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required|min:6'
         ]);
-        if (Auth::attempt($credentials)){
+        if (Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','欢迎回来!');
             return redirect()->route('users.show',[Auth::user()]);
         }else{
